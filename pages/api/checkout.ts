d369aeced81bbf3b58899102fcb92b53fe3db447
JefLeo <jefleo30@hotmail.com>
2023-12-01 15:19:30 -0300

    Checkout com variavel de produção para a URL ser modificada automaticamente

--- pages/api/checkout.ts
+++ pages/api/checkout.ts
@@ -1,46 +1,50 @@
-import {NextApiRequest, NextApiResponse} from "next";
-import {Stripe} from "stripe";
+import { NextApiRequest, NextApiResponse } from "next";
+import { Stripe } from "stripe";
 
 type Res = {
-    session?: Stripe.Checkout.Session
-    message?: string
-}
+    session?: Stripe.Checkout.Session;
+    message?: string;
+};
 
 type LineItem = {
-    price: string
-    quantity: number
-}
+    price: string;
+    quantity: number;
+};
 
 type Req = {
-    lineItems: LineItem[]
-}
+    lineItems: LineItem[];
+};
 
 export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse<Res>
 ) {
-    if (req.method != 'POST') {
-        res.status(405).json({message: 'POST method required'})
+    if (req.method != "POST") {
+        res.status(405).json({ message: "POST method required" });
     }
 
     try {
         const body: Req = JSON.parse(req.body);
 
-        const stripe = new Stripe(process.env.STRIPE_SECRET ?? '', {
-            apiVersion: '2020-08-27',
+        const stripe = new Stripe(process.env.STRIPE_SECRET ?? "", {
+            apiVersion: "2020-08-27",
         });
+
+        const baseUrl =
+            process.env.NODE_ENV === "production"
+                ? process.env.NEXT_PUBLIC_BASE_URL // Se estiver em produção, use a variável de ambiente pública
+                : "http://localhost:3000"; // Se estiver local, use o URL local
 
         const session = await stripe.checkout.sessions.create({
-            success_url: 'http://localhost:3000/success',
-            cancel_url: 'http://localhost:3000/cancel',
+            success_url: `${baseUrl}/success`,
+            cancel_url: `${baseUrl}/cancel`,
             line_items: body.lineItems,
-            mode: 'payment',
+            mode: "payment",
         });
 
-        res.status(201).json({ session })
-    }catch (e) {
+        res.status(201).json({ session });
+    } catch (e) {
         // @ts-ignore
-        res.status(500).json({message: e.message})
+        res.status(500).json({ message: e.message });
     }
-
 }
